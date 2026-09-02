0352d69f4db0af171005e5df07982b2dea700f48
SHS PC <mgermanesgilbert@gmail.com>
2026-09-02 15:09:59 +0800

    commited by belanio

---
+++ app/src/test/java/com/example/schedbuddy/quarter2/belanioFunction.java
@@ -0,0 +1,23 @@
+package com.example.schedbuddy.quarter2;
+
+import org.junit.Test;
+public class belanioFunction {
+    @Test
+
+    public void printMyProfile() {
+        // 1. THE INPUT (Storing your infos)
+        String myName = "Kiel";
+        String petName = "Brownie";
+        String favFood = "Kare-kare";
+        int myAge = 16;
+
+        // 2. THE OUTPUT (Printing your infos provided)
+
+        System.out.println("--MY DIGITAL PROFILE--");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old ");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+
+
+    }
+}
