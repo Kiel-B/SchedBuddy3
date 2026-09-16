00b6762ed2673e78ce147d354bacb01502bee0f7
Yureinlei <152169614+Yureinlei@users.noreply.github.com>
2026-09-16 14:07:57 +0800

    commited by belanio

---
+++ app/src/test/java/MiniPeta3/LogInFeature.java
@@ -0,0 +1,12 @@
+package MiniPeta3;
+
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class LogInFeature {
+    @Test
+    public void testCompleteSystemFlow(){
+
+    }
+}
