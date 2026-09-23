0ffa5f3be0e5b7253b82053b1d119bfd2ca3bd62
micsdev <micogene.pregoner01@gmail.com>
2026-09-23 14:59:01 +0800

    Commited by Pregoner Mini Peta 3

--- app/src/test/java/com/example/schedbuddy/quarter2/MiniPeta3/MainMenu.java
+++ app/src/test/java/com/example/schedbuddy/quarter2/MiniPeta3/MainMenu.java
@@ -8,21 +8,25 @@ public class MainMenu {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Variables are declared in MainMenu
+        String username = "Kiel";
+        String password = "1234";
+
+
         int choice;
 
         do {
+
             System.out.println("\n===== SCHEDBUDDY =====");
             System.out.println("1. Login Feature");
-            System.out.println("2. Notification");
-            System.out.println("3. Task Management");
-            System.out.println("4. Schedule Management");
-            System.out.println("5. Exit");
+            System.out.println("2. Exit");
 
             System.out.print("Choose an option: ");
 
             while (!scanner.hasNextInt()) {
                 System.out.println("Invalid input. Please enter a number.");
                 scanner.next();
+
                 System.out.print("Choose an option: ");
             }
 
@@ -32,33 +36,27 @@ public class MainMenu {
             switch (choice) {
 
                 case 1:
+
                     LogInFeature loginFeature = new LogInFeature();
-                    loginFeature.login();
+
+                    // Pass the variables from MainMenu
+                    loginFeature.login(username, password);
+
                     break;
 
                 case 2:
-                    NotificationFeature notification = new NotificationFeature();
-                    notification.notificationFeature();
-                    break;
 
-
-                case 3:
-                    System.out.println("\nTask Management selected.");
-                    break;
-
-                case 4:
-                    System.out.println("\nSchedule Management selected.");
-                    break;
-
-                case 5:
                     System.out.println("\nExiting SchedBuddy...");
+
                     break;
 
                 default:
-                    System.out.println("\nInvalid option. Please choose 1-5.");
+
+                    System.out.println("\nInvalid option.");
+
             }
 
-        } while (choice != 5);
+        } while (choice != 2);
 
         scanner.close();
     }
