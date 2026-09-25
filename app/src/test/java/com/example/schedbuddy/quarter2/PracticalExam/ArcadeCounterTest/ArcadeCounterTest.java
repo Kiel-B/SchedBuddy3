3622beb41b56872ea049eab969ab9040ff3bbe5a
micsdev <micogene.pregoner01@gmail.com>
2026-09-25 15:08:20 +0800

    1st Commit - Pregoner Skeleton Set up

---
+++ app/src/test/java/com/example/schedbuddy/quarter2/PracticalExam/ArcadeCounterTest/ArcadeCounterTest.java
@@ -0,0 +1,17 @@
+package com.example.schedbuddy.quarter2.PracticalExam.ArcadeCounterTest;
+
+import org.junit.Test;
+import java.util.Scanner;
+
+public class ArcadeCounterTest {
+
+    @Test
+    public void testArcadeFlow() {
+        Scanner scanner = new Scanner("");
+        start(scanner);
+    }
+
+    public void start(Scanner scanner) {
+        // Program logic will be added in later commits
+    }
+1
